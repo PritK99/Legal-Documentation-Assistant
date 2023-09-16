9eed2048aca32833202b461bb142ba668ae1674c
github_KGan31 <kavan.gandhi31@gmail.com>
2023-09-16 08:15:50 +0530

    Added 3 APIs

--- database/services.sql
+++ database/services.sql
@@ -1,7 +1,7 @@
-CREATE TABLE service (
+CREATE TABLE services (
     service_id INT AUTO_INCREMENT PRIMARY KEY,
     service_name VARCHAR(50)
 );
 
-INSERT INTO service (service_name) VALUES ("Contracts"), ("Trademark & Copyright"), ("Banking/Finance"), 
+INSERT INTO services (service_name) VALUES ("Contracts"), ("Trademark & Copyright"), ("Banking/Finance"), 
 ("Property"), ("Bonds"), ("Criminal"), ("Divorce/Family Laws");
