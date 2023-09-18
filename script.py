1d49d4afd75aa4451bec28ceb0ea326ebad99c60
Vedant <vedantnimjed@gmail.com>
2023-09-18 23:46:12 +0530

    Fixed clashing CSS color themes in InputForm, and deleted useless files & scripts
    Signed-off-by: Vedant <vedantnimjed@gmail.com>

--- script.py
+++
@@ -1,5 +0,0 @@
-import re
-test = " AGREEMENT is made this _____________day of ________ between __________M/s ______________, a Company registered under the Companies Act,____, and having its registered office at ___________ hereinafter referred to as `the Licensor' of the One Part and Mr. ._________________carrying on business of ­­­­­­­­­­­­________________ Hereinafter referred has `the Licensee' of the Other Part"
-
-print(re.split(r"_+", test))
-
