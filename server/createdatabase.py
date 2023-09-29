33a5e8c68e4868a0cbca2de5bc09b2ddd8a8664d
unknown <mihirgrathod23@gmail.com>
2023-09-29 17:21:50 +0530

    Added ques_category table

--- server/createdatabase.py
+++ server/createdatabase.py
@@ -23,7 +23,7 @@ cur = conn.cursor()
 #     img_link VARCHAR(300)
 # );''')
   
-# Insert some data into the table
+# # Insert some data into the table
 # cur.execute(
 #     '''INSERT INTO services (service_id, service_name, desciption, img_link) VALUES 
 # (1, 'Contract Documents', 'Legal contracts serve as the backbone of countless business and personal transactions, providing a formal framework for defining rights, obligations, and responsibilities between parties involved. ', 'https://res.cloudinary.com/dgi53xxbd/image/upload/v1694875757/sih_legal_docs/contract_img_ync08t.jpg'), 
@@ -59,36 +59,38 @@ cur = conn.cursor()
 #     ques_id INT PRIMARY KEY,
 #     ques_label VARCHAR(150),
 #     ques_type VARCHAR(50),
-#     ques_name VARCHAR(50)
+#     ques_name VARCHAR(50),
+#     category_id INT,
+#     FOREIGN KEY (category_id) REFERENCES ques_categories(id)
 # );
 # ''')
 
 # cur.execute('''
-# INSERT INTO input_ques (ques_id, ques_label, ques_type, ques_name) VALUES 
-# (1, 'Time', 'time', 'time'), (2, 'Day', 'text', 'day'), (3, 'Month', 'text', 'month'), (4, 'Tenant Name', 'text', 'lesse'), 
-# (5, 'Tenant State', 'text', 'lesse_state'), (6, 'Owner Name', 'text', 'lessor'), (7, 'Owner State', 'text', 'lessor_state'), 
-# (8, 'Rental contract in years', 'number', 'lease_years'), (9, 'Land Location', 'text', 'lease_loc'), 
-# (10, 'Rental agreement starts from which month', 'text', 'lease_month'), (11, 'Start year of agreement', 'number', 'lease_year'), 
-# (12, 'Monthly Rent', 'number', 'rent_monthly'), (13, 'Rate of Interest', 'text', 'interest_rate'), (14, 'Tax on Land', 'number', 'lease_text'),
-# (15, 'Payment deadline in months', 'number', 'payment_deadline'), (16, 'Witness 1 Name', 'text', 'witness_1'), (17, 'Witness 2 Name', 'text', 'witness_2'),
-# (18, 'Year', 'number', 'year'), (19, 'Vendor Name', 'text', 'vendor'),  (20, 'Vendor''s Parent Name', 'text', 'vendors_parent'),
-# (21, 'Vendor Address', 'text', 'vendor_addr'), (22, 'Purchaser''s Name', 'text', 'purchaser'), (23, 'Purchaser''s Parent Name', 'text', 'purchaser_parent'),
-# (24, 'Purchaser''s Address', 'text', 'purchaser_addr'), (25, 'House Address', 'text', 'house_addr'), (26, 'House Price', 'number', 'house_sale_price'),
-# (27, 'Advance Payment', 'number', 'advance_payment'), (28, 'Advance Payment Date', 'number', 'advance_payment_date'),
-# (29, 'Number of days after which Purchaser''s Advocate should report', 'number', 'purchaser_report_days'), 
-# (30, 'Number of days after which refund for advance payment should be given', 'number', 'refund_adv_days'),
-# (31, 'Refund for Breach of Contract', 'number', 'refund_breach'), (32, 'Name of Copyright Owner', 'text', 'copyright_owner'), 
-# (33, 'Copyright Owner''s Parent', 'text', 'copyright_owner_parent'), (34, 'Copyright Owner''s Address', 'text', 'copyright_owner_address'), 
-# (35, 'License Awardee''s Name', 'text', 'license_awardee'), (36, 'License Payment Date', 'number', 'license_payment_date'), 
-# (37, 'Agreement Date', 'date', 'agreement_date'), (38, 'Author''s Name', 'text', 'author'), (39, 'Author''s Address', 'text', 'author_addr'),
-# (40, 'Publisher''s Name', 'text', 'publisher'), (41, 'Publisher''s Address', 'text', 'publisher_addr'), (42, 'Book''s Name', 'text', 'book_name'),
-# (43, 'Deadline for Publishing the Book in months', 'number', 'publish_deadline'), (44, 'Royalty', 'number', 'royalty'), (45, 'Lender''s Address', 'text', 'lender_addr'),
-# (46, 'Lender''s Company', 'text', 'lender_company'), (47, 'Borrower''s Company', 'text', 'borrower_company'), (48, 'Borrower''s Address', 'text', 'borrower_addr'),
-# (49, 'Amount paid as Capital', 'number', 'capital_paid'), (50, 'Intercorporate loan amount', 'number', 'amount'), (51, 'Days to repay the Loan', 'number', 'days_to_repay'), (52, 'Penalised Rate of Interest', 'number', 'penalized_interest_rate'),
-# (53, 'Stock exchange on which the shares are listed', 'number', 'stock_exchange'), (54, 'Price of each share', 'number', 'rate_per_share'),
-# (55, 'Number of equity shares to be given as security', 'number', 'no_of_share'), (56, 'How often shall the Publisher submit a statement of sales to the Author', 'number', 'publish_report'), 
-# (57, 'Number of free copies to be sent to the Author', 'number', 'no_of_copies_to_author'), (58, 'Maximum copies to be sent to the media', 'number', 'media_copies_limit'),
-# (59, 'Maximum number of copies to be printed', 'number', 'print_limit'), (60, 'Maximum Price of Book on Sale', 'number', 'print_max_price');
+# INSERT INTO input_ques (ques_id, ques_label, ques_type, ques_name, category_id) VALUES 
+# (1, 'Time', 'time', 'time', 3), (2, 'Day', 'number', 'day', 3), (3, 'Month', 'text', 'month', 3), (4, 'Tenant Name', 'text', 'lesse', 1), 
+# (5, 'Tenant State', 'text', 'lesse_state', 1), (6, 'Owner Name', 'text', 'lessor', 1), (7, 'Owner State', 'text', 'lessor_state', 1), 
+# (8, 'Rental contract in years', 'number', 'lease_years', 2), (9, 'Land Location', 'text', 'lease_loc', 4), 
+# (10, 'Rental agreement starts from which month', 'text', 'lease_month', 2), (11, 'Start year of agreement', 'number', 'lease_year', 2), 
+# (12, 'Monthly Rent', 'number', 'rent_monthly', 2), (13, 'Rate of Interest', 'text', 'interest_rate', 2), (14, 'Tax on Land', 'number', 'lease_text', 2),
+# (15, 'Payment deadline in months', 'number', 'payment_deadline', 2), (16, 'Witness 1 Name', 'text', 'witness_1', 1), (17, 'Witness 2 Name', 'text', 'witness_2', 1),
+# (18, 'Year of Signing', 'number', 'year', 3), (19, 'Vendor Name', 'text', 'vendor', 1),  (20, 'Vendor''s Parent Name', 'text', 'vendors_parent', 1),
+# (21, 'Vendor Address', 'text', 'vendor_addr', 1), (22, 'Purchaser''s Name', 'text', 'purchaser', 1), (23, 'Purchaser''s Parent Name', 'text', 'purchaser_parent', 1),
+# (24, 'Purchaser''s Address', 'text', 'purchaser_addr', 1), (25, 'House Address', 'text', 'house_addr', 4), (26, 'House Price', 'number', 'house_sale_price', 2),
+# (27, 'Advance Payment', 'number', 'advance_payment', 2), (28, 'Advance Payment Date', 'number', 'advance_payment_date', 2),
+# (29, 'Number of days after which Purchaser''s Advocate should report', 'number', 'purchaser_report_days', 2), 
+# (30, 'Number of days after which refund for advance payment should be given', 'number', 'refund_adv_days', 2),
+# (31, 'Refund for Breach of Contract', 'number', 'refund_breach', 2), (32, 'Name of Copyright Owner', 'text', 'copyright_owner', 1), 
+# (33, 'Copyright Owner''s Parent', 'text', 'copyright_owner_parent', 1), (34, 'Copyright Owner''s Address', 'text', 'copyright_owner_address', 1), 
+# (35, 'License Awardee''s Name', 'text', 'license_awardee', 1), (36, 'License Payment Date', 'date', 'license_payment_date', 2), 
+# (37, 'Agreement Date', 'date', 'agreement_date', 3), (38, 'Author''s Name', 'text', 'author', 1), (39, 'Author''s Address', 'text', 'author_addr', 1),
+# (40, 'Publisher''s Name', 'text', 'publisher', 1), (41, 'Publisher''s Address', 'text', 'publisher_addr', 1), (42, 'Book''s Name', 'text', 'book_name', 5),
+# (43, 'Deadline for Publishing the Book in months', 'number', 'publish_deadline', 2), (44, 'Royalty', 'number', 'royalty', 2), (45, 'Lender''s Address', 'text', 'lender_addr', 1),
+# (46, 'Lender''s Company', 'text', 'lender_company', 1), (47, 'Borrower''s Company', 'text', 'borrower_company', 1), (48, 'Borrower''s Address', 'text', 'borrower_addr', 1),
+# (49, 'Amount paid as Capital', 'number', 'capital_paid', 2), (50, 'Intercorporate loan amount', 'number', 'amount', 2), (51, 'Days to repay the Loan', 'number', 'days_to_repay', 2), (52, 'Penalised Rate of Interest', 'number', 'penalized_interest_rate', 2),
+# (53, 'Stock exchange on which the shares are listed', 'text', 'stock_exchange', 2), (54, 'Price of each share', 'number', 'rate_per_share', 2),
+# (55, 'Number of equity shares to be given as security', 'number', 'no_of_share', 2), (56, 'How often shall the Publisher submit a statement of sales to the Author', 'number', 'publish_report', 2), 
+# (57, 'Number of free copies to be sent to the Author', 'number', 'no_of_copies_to_author', 2), (58, 'Maximum copies to be sent to the media', 'number', 'media_copies_limit', 2),
+# (59, 'Maximum number of copies to be printed', 'number', 'print_limit', 2), (60, 'Maximum Price of Book on Sale', 'number', 'print_max_price', 5);
 # ''')
 
 # cur.execute('''
@@ -119,11 +121,24 @@ cur = conn.cursor()
 
 # ''')
 
-cur.execute(
-    '''SELECT *
-    FROM form_queries;''')
-data = cur.fetchall()
-print(data)
+# category table queries
+# cur.execute('''
+# CREATE TABLE IF NOT EXISTS ques_categories (
+#     id SERIAL PRIMARY KEY,
+#     category_name VARCHAR(70)
+# );
+# ''')
+
+# cur.execute('''
+
+#     INSERT INTO ques_categories (id, category_name) VALUES
+#     (1, 'Parties'), (2, 'Terms and Payment'), (3, 'Signing Date'), (4, 'Property Details'), (5, 'Intellectual Property Details')
+
+# ''')
+
+# cur.execute('''DROP TABLE services;''')
+# data = cur.fetchall()
+# print(data)
 # commit the changes
 conn.commit()
   
