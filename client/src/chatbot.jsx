03673733fdf758be7d0b76d996db28e52962a9a3
shark-21 <shardulkhade88@gmail.com>
2023-09-29 16:04:17 +0530

    Chatbot final

--- client/src/chatbot.jsx
+++ client/src/chatbot.jsx
@@ -12,16 +12,15 @@ const Chatbot = () => {
        
       
         "composerPlaceholder": "Ask your legal query",
-        "botConversationDescription": "This chatbot was built surprisingly fast with Botpress",
-        "botId": "42cc66eb-b6fb-44f8-9d2a-2b732298f460",
-        "hostUrl": "https://cdn.botpress.cloud/webchat/v1",
-        "messagingUrl": "https://messaging.botpress.cloud",
-        "clientId": "42cc66eb-b6fb-44f8-9d2a-2b732298f460",
-        "lazySocket": true,
-        "botName": "LegalAdvisor",
-        "stylesheet": "https://webchat-styler-css.botpress.app/prod/e445f64c-9988-4529-b187-ac0c105ee60f/v69236/style.css",
-        "frontendVersion": "v1",
-        "showPoweredBy": true
+      "botConversationDescription": "This chatbot was built surprisingly fast with Botpress",
+      "botId": "42cc66eb-b6fb-44f8-9d2a-2b732298f460",
+      "hostUrl": "https://cdn.botpress.cloud/webchat/v1",
+      "messagingUrl": "https://messaging.botpress.cloud",
+      "clientId": "42cc66eb-b6fb-44f8-9d2a-2b732298f460",
+      "lazySocket": true,
+      "botName": "LegalAdvisor",
+      "stylesheet": "https://webchat-styler-css.botpress.app/prod/code/a1ecdf9f-eecd-49d0-af7d-626060e2799b/v30410/style.css",
+      "frontendVersion": "v1"
         
       })
     }
