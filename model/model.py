ffadbbde407eafb67b63a38a56867649aa1a859f
Prit Kanadiya <103832825+PritK99@users.noreply.github.com>
2023-11-27 17:49:12 +0530

    Adding model architecture

---
+++ model/model.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+
+class Chatbot(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Linear(input_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, output_size),
+        )
+
+    def forward(self, x):
+        self.model(x)
