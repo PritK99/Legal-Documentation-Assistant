238500083e06560310261ae4ae0381c6afd1bab8
Prit Kanadiya <103832825+PritK99@users.noreply.github.com>
2023-11-27 18:30:39 +0530

    changing hidden layer structure

--- model/model.py
+++ model/model.py
@@ -5,11 +5,11 @@ class Chatbot(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super().__init__()
         self.model = nn.Sequential(
-            nn.Linear(input_size, hidden_size),
+            nn.Linear(input_size, hidden_size[0]),
             nn.ReLU(),
-            nn.Linear(hidden_size, hidden_size),
+            nn.Linear(hidden_size[0], hidden_size[1]),
             nn.ReLU(),
-            nn.Linear(hidden_size, output_size),
+            nn.Linear(hidden_size[1], output_size),
         )
 
     def forward(self, x):
