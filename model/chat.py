63eaf6d70d6299a281519cbfbaa489b8cb0ad7a4
Prit Kanadiya <103832825+PritK99@users.noreply.github.com>
2023-11-28 19:38:47 +0530

    adding working chatbot

--- model/chat.py
+++ model/chat.py
@@ -1,6 +1,5 @@
 import random
 import json
-
 import torch
 
 from model import Chatbot
@@ -11,30 +10,29 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 with open('intents.json', 'r') as json_data:
     intents = json.load(json_data)
 
-FILE = "trained_model.pth"
-data = torch.load(FILE)
+save_path = "trained_model.pth"
+data = torch.load(save_path)
 
 input_size = data["input_size"]
 hidden_size = data["hidden_size"]
 output_size = data["output_size"]
-all_words = data['all_words']
+words = data['words']
 tags = data['tags']
 model_state = data["model_state"]
-
 model = Chatbot(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
+
 model.eval()
 
-bot_name = "Sam"
+bot_name = "DocBuddy"
 print("Let's chat! (type 'quit' to exit)")
 while True:
-    # sentence = "do you use credit cards?"
     sentence = input("You: ")
     if sentence == "quit":
         break
 
     sentence = tokenize(sentence)
-    X = bag_of_words(sentence, all_words)
+    X = bag_of_words(sentence, words)
     X = X.reshape(1, X.shape[0])
     X = torch.from_numpy(X).to(device)
 
@@ -50,4 +48,4 @@ while True:
             if tag == intent["tag"]:
                 print(f"{bot_name}: {random.choice(intent['responses'])}")
     else:
-        print(f"{bot_name}: I do not understand...")
+        print(f"{bot_name}: I am unable to process the given query. Please try again")
