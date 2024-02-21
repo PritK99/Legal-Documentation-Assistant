1b3393723e2b69dc3153b377a7394afae3f28ace
Prit Kanadiya <103832825+PritK99@users.noreply.github.com>
2024-02-21 12:19:41 +0530

    Solving import error

--- model/chatbot.py
+++ model/chatbot.py
@@ -1,9 +1,16 @@
 import random
 import json
 import torch
+import os
+import sys
 
 from model import Chatbot
 from utils import bag_of_words, tokenize
+
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
