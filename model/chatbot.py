0c1c5f2ed9aa24f7950189c0001f2bf7e69b4ca2
Prit Kanadiya <103832825+PritK99@users.noreply.github.com>
2024-02-21 11:57:10 +0530

    providing interface for website chatbot

---
+++ model/chatbot.py
@@ -0,0 +1,46 @@
+import random
+import json
+import torch
+
+from model import Chatbot
+from utils import bag_of_words, tokenize
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def get_response(sentence):
+
+    with open('intents.json', 'r') as json_data:
+        intents = json.load(json_data)
+
+    save_path = "trained_model.pth"
+    data = torch.load(save_path)
+
+    input_size = data["input_size"]
+    hidden_size = data["hidden_size"]
+    output_size = data["output_size"]
+    words = data['words']
+    tags = data['tags']
+    model_state = data["model_state"]
+    model = Chatbot(input_size, hidden_size, output_size).to(device)
+    model.load_state_dict(model_state)
+
+    model.eval()
+
+    sentence = tokenize(sentence)
+    X = bag_of_words(sentence, words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+    if prob.item() > 0.75:
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                return random.choice(intent['responses'])
+    else:
+        return "I am unable to process the given query. Please try again"
