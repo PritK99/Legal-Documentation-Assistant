eb301066371039fb2e87de54119cd8c577e2725b
Prit Kanadiya <103832825+PritK99@users.noreply.github.com>
2024-02-21 12:26:01 +0530

    Removing circular dependency

--- model/bot.py
+++ model/bot.py
@@ -7,7 +7,6 @@ import sys
 from model import Chatbot
 from utils import bag_of_words, tokenize
 
-
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
